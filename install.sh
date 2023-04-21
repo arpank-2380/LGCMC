e83268701c636fec06643c80d90c73afda9a87ea
Arpan Kundu <arpank@uchicago.edu>
2023-04-21 18:27:38 -0500

    install.sh modified

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 root_dir=`pwd`
 binary_location=${root_dir}/bin
 #compiler_module='ic111_20100414'     # You may have to change this to look for your intel fortran compiler
-source /opt/intel/oneapi/setvars.sh 
+#source /opt/intel/oneapi/setvars.sh 
 cd ${root_dir}/source
    make clean
    make all
